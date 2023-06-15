58518fb7bed8f70bb0fd58a25e622b5192c6d288
DoyoonKim <kdy0096@gmail.com>
2023-06-15 10:42:08 +0900

    Initial Commit

---
+++ Ubie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ubie
+//
+//  Created by YoonieMac on 2023/06/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
